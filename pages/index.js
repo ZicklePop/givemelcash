2c3f6e632421872f67569b47a31b2d6adb4430f9
Melanie Bilbrey <melanie.bilbrey@creditkarma.com>
2020-02-12 12:07:09 -0800

    init

---
+++ pages/index.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import Layout from '../components/layout'
+import ListLink from '../components/list-link'
+
+const cx = {
+  main: 'vh-100 dt w-100',
+  container: 'dtc v-mid tc',
+  article: 'mw5 center sans-serif f3 fw2 ph2',
+  h1: 'lh-title fw2 f2',
+  h1a: 'no-underline',
+  ul: 'list pl0 tl',
+  liLast: 'lh-title mb2 mt4',
+  img: 'br-100 h4 w4 center dib ma3'
+}
+
+const Index = () => (
+  <Layout className={cx.main}>
+    <div className={cx.container}>
+      <article className={cx.article}>
+        <img
+          className={cx.img}
+          src='/static/uglytarantula-commission.jpg'
+          title='commission by @uglytarantula'
+        />
+        <h1 className={cx.h1}>
+          <a
+            className={cx.h1a}
+            href='/'
+          >
+            {'Give Mel Cash'}
+          </a>
+        </h1>
+
+        <ul className={cx.ul}>
+          <ListLink
+            text='Ko-fi'
+            rel='me'
+            url='https://ko-fi.com/melkat'
+            qrCode=''
+          />
+          <ListLink
+            text='Paypal'
+            rel='me'
+            url='https://www.paypal.me/melaniekat'
+            qrCode=''
+          />
+          <ListLink
+            text='Venmo'
+            rel='me'
+            url='https://venmo.com/ZicklePop'
+            qrCode=''
+          />
+          <ListLink
+            text='Square Cash'
+            rel='me'
+            url='https://cash.app/$melkat39'
+            qrCode=''
+          />
+        </ul>
+      </article>
+    </div>
+  </Layout>
+)
+
+export default Index
