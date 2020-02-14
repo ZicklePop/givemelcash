315888fb302524091dbef5e463e8d10a2dec822d
Melanie Kat <318003+ZicklePop@users.noreply.github.com>
2020-02-13 19:53:00 -0800

    better accessibility for giving me money

--- components/qr-code.js
+++ components/qr-code.js
@@ -2,20 +2,25 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const cx = {
-  container: 'absolute absolute--fill bg-center contain bg-white z-1 pointer invert'
+  container: 'vh-100 w-100 dt absolute absolute--fill bg-white pointer invert',
+  imgContainer: 'dtc v-mid tc',
+  img: ''
 }
 
-const QRCode = ({ className, src, onClick }) => (
-  <div
-    className={className}
-    onClick={onClick}
-    style={{ backgroundImage: `url(${src})` }}
-  />
+const QRCode = ({ className, src }) => (
+  <div className={className}>
+    <div className={cx.imgContainer}>
+      <img
+        src={src}
+        alt={'Press to go back.'}
+        className={cx.img}
+      />
+    </div>
+  </div>
 )
 
 QRCode.propTypes = {
   className: PropTypes.string,
-  onClick: PropTypes.func,
   src: PropTypes.string
 }
 
